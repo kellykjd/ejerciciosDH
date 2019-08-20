2f31fb60f8e69cec5fe123a1018c6a7c4d0f6ab2
Kelly Jimenez <36087642+kellykjd@users.noreply.github.com>
2019-08-20 17:47:47 -0300

    Update Main.java

--- Clase 04/atletasYPrueba/src/com/company/Main.java
+++ Clase 04/atletasYPrueba/src/com/company/Main.java
@@ -4,5 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+Atleta atlUno = new Atleta("Luis",30,60);
+Atleta atlDos = new Atleta("Marcos",60,70);
+
+Prueba testOne = new Prueba(20,60);
+Prueba testTwo = new Prueba(30,70);
+Prueba testThree = new Prueba(40,80);
+
+testOne.puedeRealizar(atlUno);
+testOne.puedeRealizar(atlDos);
+testTwo.puedeRealizar(atlUno);
+testTwo.puedeRealizar(atlDos);
+testThree.puedeRealizar(atlUno);
+testThree.puedeRealizar(atlDos);
+
+
+
     }
 }
