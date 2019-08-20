eb89406533e6ec2a23a22ab2ea9c9ece61da9aad
Kelly Jimenez <36087642+kellykjd@users.noreply.github.com>
2019-08-20 17:47:19 -0300

    Update Atleta.java

--- Clase 04/atletasYPrueba/src/com/company/Atleta.java
+++ Clase 04/atletasYPrueba/src/com/company/Atleta.java
@@ -1,4 +1,33 @@
 package com.company;
 
 public class Atleta {
+
+    //ATRIBUTOS
+    private String nombre;
+    private Integer nivel;
+    private Integer energia;
+
+    //CONSTRUCTOR
+
+    public Atleta(String nombre, Integer nivel, Integer energia) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.energia = energia;
+    }
+
+    //Metodo
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getNivel() {
+        return nivel;
+    }
+
+    public Integer getEnergia() {
+        return energia;
+    }
 }
+
