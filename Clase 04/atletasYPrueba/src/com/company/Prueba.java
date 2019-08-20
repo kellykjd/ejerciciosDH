a84d8dc9e11ba6c72ccc63b5acae727e2dd6a09c
Kelly Jimenez <36087642+kellykjd@users.noreply.github.com>
2019-08-20 17:48:10 -0300

    Update Prueba.java

--- Clase 04/atletasYPrueba/src/com/company/Prueba.java
+++ Clase 04/atletasYPrueba/src/com/company/Prueba.java
@@ -1,4 +1,30 @@
 package com.company;
 
 public class Prueba {
+
+    //ATRIBUTOS
+    private Integer dificultadRequerida;
+    private Integer energiaRequerida;
+    private Atleta atleta;
+
+    //CONSTRUCTOR
+
+    public Prueba(Integer dificultadRequerida, Integer energiaRequerida) {
+        this.dificultadRequerida = dificultadRequerida;
+        this.energiaRequerida = energiaRequerida;
+    }
+
+    //METODOS
+
+    public Boolean puedeRealizar(Atleta atleta){
+        if (atleta.getNivel()>=dificultadRequerida && atleta.getEnergia()>=energiaRequerida){
+            System.out.println("El atleta "+atleta.getNombre()+" puede realizar la prueba.");
+            return true;
+        }else{
+            System.out.println("El atleta "+atleta.getNombre()+" no puede realizar la prueba.");
+            return false;
+        }
+
+
+    }
 }
